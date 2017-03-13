700157e6d17975ce00e246585285195620632e0d
Maxim Zaitsev <zaytsev.max90@gmail.com>
2017-03-13 09:33:26 +0300

    1.0: APLS-83
    МОБИЛЬНАЯ ВЕРСИЯ

--- customers/weekend_discount/index.php
+++ customers/weekend_discount/index.php
@@ -3,6 +3,6 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Скидка выходного дня");
 ?>
 
-	<img src="../img/weekend_discount.png">
+	<img src="../img/weekend_discount.png" class="AplsFullImg">
 	<p class="TextP">Внимание! Каждое воскресенье скидка до 10% для всех розничных покупателей.</p>
 	<p class="TextP InfoText">* Скидка не распространяется на товары, участвующие в иных акциях или распродажах.</p><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
