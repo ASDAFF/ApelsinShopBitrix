8acb4c9376dbef10e8fe29f8c6b68dc3c28c08b9
Olga Rjabchikova <olga.rjabchicova@gmail.com>
2017-04-11 12:17:55 +0300

    1.0: APLS-103 Переименовать кнопку в Бысром заказе
    Переименовать кнопку в Быстром заказе с Купить на Заказать

--- bitrix/components/altop/buy.one.click/templates/cart/lang/ru/template.php
+++ bitrix/components/altop/buy.one.click/templates/cart/lang/ru/template.php
@@ -5,6 +5,6 @@ $MESS["MFT_BOC_TEL"] = "Телефон";
 $MESS["MFT_BOC_EMAIL"] = "Email";
 $MESS["MFT_BOC_MESSAGE"] = "Сообщение";
 $MESS["MFT_BOC_CAPTCHA"] = "Код с картинки";
-$MESS["MFT_BOC_BUY"] = "Купить";
+$MESS["MFT_BOC_BUY"] = "Заказать";
 $MESS["MFT_BOC_DESCRIPTION"] = "Спасибо за ваш заказ. В ближайшее время с вами свяжется оператор.";
 ?>
