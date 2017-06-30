c72f43e7bc44076bc2d1bfcae03847262da114eb
Maxim Zaitsev <zaytsev.max90@gmail.com>
2017-06-30 14:21:00 +0300

    Исправление строки подтверждения соглашения при оформлении заказа

--- bitrix/templates/elektro_flat/components/bitrix/sale.order.ajax/.default/agreement.php
+++ bitrix/templates/elektro_flat/components/bitrix/sale.order.ajax/.default/agreement.php
@@ -1,4 +1,7 @@
 <div class="block">
-    Нажимая кнопку, я принимаю <a href="http://<?=$_SERVER['SERVER_NAME'];?>/payments/terms_of_use/">условия использования сервиса</a>
+    Нажимая кнопку, я принимаю
+    <a href="http://<?=$_SERVER['SERVER_NAME'];?>/payments/terms_of_use/">пользовательское соглашение</a>
+    и условия
+    <a href="http://<?=$_SERVER['SERVER_NAME'];?>/payments/offer/">оферты</a>.
 </div>
 <div class="clr"></div>
